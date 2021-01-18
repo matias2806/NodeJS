243a4517efb1241f8404c38579182a3b011f9807
gliria <gliria@quaresitsolutions.com>
2021-01-18 20:35:38 -0300

    initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    //res.sendFile(path.join(__dirname, '/views/index.html')); esto es para HTML
+    res.render('index.html', {title : 'First Website'});
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact.html', {title: 'Contact Page.'});
+});
+
+
+module.exports = router;
