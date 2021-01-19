f7a58e74d4972eba48c0f261adfbf320585610bd
gliria <gliria@quaresitsolutions.com>
2021-01-19 17:09:35 -0300

    Se agregan metodos HTTP, Favicon y json de postman para realizar pruebas ademas algunos fixs chicos

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 
 //settings
+app.set('appName', 'Matias Palmieri NodeJS Quares');
 app.set('port', 3000);
 app.set('views', path.join(__dirname, 'views')); //esto para trabajar con ejs y le marcas donde van a estar tus views
 app.engine('html', require('ejs').renderFile); //Aca le digo que quiero trabajar con archivos HTML pero que se procesen con ejs osea voy a mantener lo que me brinda ejs
@@ -10,7 +11,7 @@ app.set('view engine', 'ejs'); //esto es para aclarar que vas a trabajar con ejs
 
 
 //midlewares
-
+app.use(express.json());
 
 //routes
 app.use(require('./routes/'));
@@ -20,5 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //listening the server
 app.listen(app.get('port'), () => {
+    console.log(app.get('appName'));
     console.log('Server on port', app.get('port'), 'Visit => http://localhost:3000/');
 })
+
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,34 @@
 const express = require ('express');
 const router = express.Router();
 
+
+
 router.get('/', (req, res) => {
     //res.sendFile(path.join(__dirname, '/views/index.html')); esto es para HTML
     res.render('index.html', {title : 'First Website'});
 });
 
 router.get('/contact', (req, res) => {
-    res.render('contact.html', {title: 'Contact Page.'});
+    res.render('contact.html', {title: 'Contact Page. peticion post'});
 });
 
+
+router.post('/formContact', (req, res) => {
+    console.log(req.body);
+    console.log(res.body );
+    res.send('POST RECIBE');
+});
+
+router.put('/putContact/:id', (req, res) => {
+    console.log(req.body);
+    console.log(res.body );
+    res.send(`PUT RECIBE \n\n Usuario modificado = ${req.params.id} \n  con el mail = ${req.body.mail} y el mensaje = ${req.body.mensaje}`);
+});
+
+router.delete('/deleteContact/:id', (req, res) => {
+    console.log(req.body);
+    console.log("id =>", req.params);
+    res.send(`DELETE RECIBE \n\n User ${req.params.id} deleted`);
+});
 
 module.exports = router;
