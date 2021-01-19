5a30f7f92102570282f3effe8b98143cc35f2e44
gliria <gliria@quaresitsolutions.com>
2021-01-19 14:43:36 -0300

    fixs

--- README.txt
+++ README.txt
@@ -3,5 +3,10 @@ Mi primer App en NodeJS
 
 Para ejecutar el programa ejecutar en la consola
 
+1) npm install
 
-nodemon src
+2) npm run dev
+
+En el caso que no le permita ejecutar este comando pruebe con 
+
+2A) node src
--- src/index.js
+++ src/index.js
@@ -20,5 +20,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //listening the server
 app.listen(app.get('port'), () => {
-    console.log('Server on port', app.get('port'));
+    console.log('Server on port', app.get('port'), 'Visit => http://localhost:3000/');
 })
